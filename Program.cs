46cf015b6c3a15c02bef1b210f810f42ee9d87ce
Darya Vaitsiakhouskaya <darya.vaitsiakhouskaya@mail.ru>
2023-02-27 22:53:02 +0300

    добавила существующие данные

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿//Task 1
+ /*Напишите программу, которая на вход принимает два числа и 
+ выдаёт, какое число большее, а какое меньшее.*/
+
+/*Console.WriteLine("input a first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("input a second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number1 > number2) 
+{ 
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.WriteLine(number2);
+}*/
+/*Task 2 Напишите программу, которая принимает 
+на вход три числа и выдаёт максимальное из этих чисел.*/
+
+/*Console.WriteLine("input a first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("input a second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("input a third number");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number1 > number2) 
+{ 
+    Console.WriteLine(number1);
+}
+if (number2 > number3)
+{
+    Console.WriteLine(number2);
+}
+else 
+{
+  Console.WriteLine(number3);
+}*/
+/*Task 3 
+Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).*/
+/*Console.WriteLine("input a positive number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{  
+   Console.WriteLine("да");
+}
+else
+  {
+    Console.WriteLine("нет");
+  }*/
+  /*Task 4 
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+/*Console.WriteLine("input a positive number");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while (current <= num) 
+
+ { if (current % 2 == 0)
+  Console.Write(current + " ");
+  current ++;}
+  */
+ 
+
